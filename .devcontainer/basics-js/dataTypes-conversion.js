711d932a8a03e30807d2acd32c1a238e64529cf8
divansh-529 <divanshsachdev@gmail.com>
2024-12-08 11:59:06 +0000

    conversion done and comparision started

--- .devcontainer/basics-js/dataTypes-conversion.js
+++ .devcontainer/basics-js/dataTypes-conversion.js
@@ -1,15 +1,33 @@
-let score = null; // it is string
-console.log(typeof score); // it will give the output string
-let valueInNumber = Number(score); // it will change the number which value is in string to number strictly
-console.log(typeof valueInNumber); // it will give the output in number
-console.log(valueInNumber); // it is giving NaN (not a number because we are not giving the value which can be changed to number)
+// let score = null; // it is string
+// console.log(typeof score); // it will give the output string
+// let valueInNumber = Number(score); // it will change the number which value is in string to number strictly
+// console.log(typeof valueInNumber); // it will give the output in number
+// console.log(valueInNumber); // it is giving NaN (not a number because we are not giving the value which can be changed to number)
 
 // 1=true
 //0 = false
 
-let sumNumber = 33;
-console.log("")
+// let sumNumber = 33;
+// console.log("")
 
+//**********************operation***************************
+// let number = 12
+// let negNUmber = -number
+// console.log(negNUmber);
 
+// console.log(2+2); //4
+// console.log(2-2); //0
+// console.log(2*2); //4
+// console.log(2/2);//1
+// console.log(2**2);//4
+// console.log(2%2);//0
 
+// adding of strings
 
+// let str1= "hello"
+// let str2 = "divansh"
+// let str3 = str1+" "+str2;
+// console.log(str3);
+
+// let num1 = (num2 = num3 = 2 + 2);
+// console.log(num1);
