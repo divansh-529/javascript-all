3a60e738e799097b27b7bf02e8963799b9e74d06
divansh-529 <divanshsachdev@gmail.com>
2024-12-08 18:40:19 +0000

    math and num

---
+++ .devcontainer/nums&math/maths.js
@@ -0,0 +1,21 @@
+// console.log(Math);
+// console.log(Math.abs(-4)); // convert neggitive intiger to positive
+// console.log(Math.round(-34.4)); // converts value to nearest roundof 
+// console.log(Math.ceil(34.8)); // converts value to nearest next nondecimeled number
+// console.log(Math.floor(38.4)); // converts value to prev number
+// console.log(Math.min(4,5,5,6,7778,8)); // used to find minimum value
+// console.log(Math.max(4,5,5,6,7778,8)); // used to find maximum value
+
+
+// console.log((Math.random()*10)+1)
+
+
+// to find random no between 2 things we use 
+console.log(Math.floor(Math.random() * 50));
+
+
+
+
+
+
+
---
+++ .devcontainer/nums&math/numbers.js
@@ -0,0 +1,6 @@
+const score = 800;
+const newScore = new Number(800);
+console.log(newScore);
+console.log(score);
+console.log(  score.toFixed(0));
+
