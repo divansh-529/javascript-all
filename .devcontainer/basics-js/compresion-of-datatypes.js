5189ecdd40de9165d6da4f447892e64055a75937
divansh-529 <divanshsachdev@gmail.com>
2024-12-08 13:50:14 +0000

    compreasion done

---
+++ .devcontainer/basics-js/compresion-of-datatypes.js
@@ -0,0 +1,12 @@
+// console.log(2 < 1); //  greater then
+// console.log(2 <= 1); //  greater then equal to
+// console.log(2 < 1); // smaller then
+// console.log(2 <= 1); // smaller then equal to
+// console.log(2 == 1); // equal to
+// console.log(2 != 1); // not equal to
+
+
+// null is converted to 0  in some conversion
+// undefined always givee the value of false
+
+// === is the strich check always echeck both operator and value
