e3674933345307331d42a83ac3ac66a2318be3af
divansh-529 <divanshsachdev@gmail.com>
2024-12-08 08:29:09 +0000

    variable.js

--- .devcontainer/basics-js/console.js
+++ .devcontainer/basics-js/console.js
@@ -1,1 +1,2 @@
 console.log("divansh")
+console.log("divansh")
---
+++ .devcontainer/basics-js/variable.js
@@ -0,0 +1,8 @@
+const accountId = 14455
+let accountEmail = "divanshsachdev@gmail.com"
+var accountPassword = "divansh@123"
+accountCity = "Hisar"
+
+
+// accountId =2
+console.table[ accountEmail, accountId ,  accountPassword]
