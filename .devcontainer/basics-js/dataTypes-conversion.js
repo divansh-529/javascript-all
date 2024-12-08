b626cc46da4bada6cc878fc772a974309fbbac96
divansh-529 <divanshsachdev@gmail.com>
2024-12-08 11:32:34 +0000

    conversion of data-types

---
+++ .devcontainer/basics-js/dataTypes-conversion.js
@@ -0,0 +1,15 @@
+let score = null; // it is string
+console.log(typeof score); // it will give the output string
+let valueInNumber = Number(score); // it will change the number which value is in string to number strictly
+console.log(typeof valueInNumber); // it will give the output in number
+console.log(valueInNumber); // it is giving NaN (not a number because we are not giving the value which can be changed to number)
+
+// 1=true
+//0 = false
+
+let sumNumber = 33;
+console.log("")
+
+
+
+
---
+++ .devcontainer/basics-js/datatypes.js
@@ -0,0 +1,7 @@
+let name = "divansh sachdev"; // string
+let age = 17; // number
+let isLogIn = false; // boolian
+let bigNumber = 7872*905805808; // bigINt
+let childName ; // undefined 
+let grandChild = null; // null
+
