c38b8fcc6e58ac46498772e3156eab599656e6d9
divansh-529 <divanshsachdev@gmail.com>
2024-12-09 06:35:48 +0000

    date done

---
+++ .devcontainer/date&time/date.js
@@ -0,0 +1,9 @@
+let myDate  = new Date()
+// console.log(myDate);
+
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toJSON);
+let newdate = new Date("01-14-2024")
+console.log(newdate.getTime().toString());
+
