463a229e5363f84945752cc0e9baa03a447b3627
divansh-529 <divanshsachdev@gmail.com>
2024-12-09 10:54:52 +0000

    array done

---
+++ .devcontainer/array/advance_Array.js
@@ -0,0 +1,22 @@
+// const myArr = [ "anju","lavish", "divanhs" , "sachdev"]
+// const myArr2 = [ "anju","lavish", "divanhs" , "sachdev"]
+// const myArr3 = myArr.concat(myArr2)
+// const myArr3 = [ ...myArr,...myArr2]
+
+// console.log(myArr3);
+
+// const another_array = [1,2,3,[1,2,3,[1,2,3]]]
+// console.log(another_array.flat(Infinity));
+
+score1= 100;
+score2= 200;
+score3= 300;
+
+console.log(Array.isArray(Array));
+
+
+
+
+
+
+
---
+++ .devcontainer/array/basics-array.js
@@ -0,0 +1,22 @@
+// let myArr = new Array(1,2,3,4,5)
+// myArr.push("i love you")
+// myArr.pop()
+// myArr.unshift(0)
+// myArr.shift()
+// myArr.includes(5)
+// let arr = myArr.join()
+
+// console.log(arr);
+
+let myArr = new Array(1,2,3,4,5)
+console.log(myArr);
+console.log(myArr.slice(1 ,3));
+console.log(myArr.splice(1,3,1,2,23));
+console.log(myArr);
+ 
+
+
+
+
+
+
